1c9b5b1597bf6623449d1461ae6557ec861995c8
fitrinanur <missfitrina@gmail.com>
2018-02-23 09:49:13 +0700

    - modified index and show method for show API product in product controller - create new product collection - create new product resource

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return[
+            'name'          => $this->name,
+            'description'   => $this->detail,
+            'prices'        => $this->price,
+            'stock '        => $this->stock == 0 ? 'Out Of Stock' : $this->stock,
+            'discount'      => $this->discount,
+            'totalPrice'    => round((1-($this->discount/100)) * $this->price, 2),
+            'rating'        => $this->review->count() > 0 ? round($this->review->sum('star')/$this->review->count(),2):'No Rating',
+            'href'          => [
+                'reviews'   => route('review.index',$this->id)
+            ]
+        ];
+    }
+}
